b602598c32a63fa8c74338ccf0b822403c0e92d8
wangkun <wangkun164719@gmail.com>
2026-06-29 23:59:24 +0800

    Fix local setup and dependency hygiene

--- src/components/BirthPickerField.jsx
+++ src/components/BirthPickerField.jsx
@@ -1,4 +1,4 @@
-﻿import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { CalendarIcon, ClockIcon } from "./Icons";
 
--- src/pages/InfoFormPage.jsx
+++ src/pages/InfoFormPage.jsx
@@ -1,4 +1,4 @@
-﻿import { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   DatePickerField,
