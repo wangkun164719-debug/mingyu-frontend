6540840fe75caa5b5c08204d25aad788ee9dfee1
wangkun164719-debug <wangkun164719@gmail.com>
2026-06-29 23:01:51 +0800

    Optimize new user conversion flow

--- src/components/PageSection.jsx
+++ src/components/PageSection.jsx
@@ -1,3 +1,7 @@
-export default function PageSection({ className = "", children }) {
-  return <section className={`mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 ${className}`}>{children}</section>;
+export default function PageSection({ className = "", children, ...props }) {
+  return (
+    <section {...props} className={`mx-auto max-w-6xl px-4 sm:px-6 lg:px-8 ${className}`}>
+      {children}
+    </section>
+  );
 }
